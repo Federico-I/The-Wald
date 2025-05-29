31a99c0fe0dc9d76c9986982dab01a362efe8713
Federico-I <threxos.48@gmail.com>
2025-05-29 23:24:32 +0100

    styled-comps-setUp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+function App() {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default App;
