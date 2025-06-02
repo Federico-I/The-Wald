2d82eeccba543ee580deddbd24410029a9747b24
Federico-I <threxos.48@gmail.com>
2025-06-02 18:01:58 +0100

    new-CSS-style-file

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,12 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: red;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
 const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
 `;
 
 const StyledApp = styled.div`
