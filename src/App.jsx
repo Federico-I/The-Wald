0d0be9a8367cc3f1055221e0845e3736122235ea
Federico-I <threxos.48@gmail.com>
2025-06-06 01:43:50 +0100

    settingup-app-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import Input from './ui/Input';
-import Button from './ui/Button';
-import Heading from './ui/Heading';
-import createGlobalStyle from 'styled-components';
-import Row from './ui/Row';
-
-const StyledApp = styled.div`
-  background-color: green;
-  padding: 20px;
-`;
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row typeof='vertical'>
-          <Row typeof='horizontal'>
-            <Heading as='h1'>The Wald</Heading>
-          
-            <div>
-              <Heading as='h2'>Check In and Out</Heading>
-              <Button variation="primary" size="medium" onClick={() => alert("Are you sure?")}>Check In</Button>
-              <Button variation="secondary" size="small" onClick={() => alert("Are you sure?")}>Check Out</Button>
-            </div>
-          </Row>
-
-          <Row typeof='vertical'>
-            <Heading as='h3'>Form</Heading>
-            <Input type="number" placeholder='Number of guests'/>
-            <Input type="number" placeholder='Number of guests'/>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
-}
+    <div>App</div>
+  )
+};
 
 export default App;
