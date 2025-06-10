862cd7f5e4703d374a49fc52c5315e5d9a36bef9
Federico-I <threxos.48@gmail.com>
2025-06-10 00:26:50 +0000

    Header-styledHeaderComp

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const StyledHeader = styled.header`
+  background-color: orange;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px sold var(--color-grey-100);
+`;
+
 function Header() {
   return (
     <div>Header</div>
