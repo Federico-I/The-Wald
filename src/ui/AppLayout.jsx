08810c4c395665e99a2d3f8d39fa42d8285d7569
Federico-I <threxos.48@gmail.com>
2025-06-10 00:23:39 +0000

    applayout-comps-HeaderStyledCopm

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../pages/Header';
+import styled from 'styled-components';
+
+const main = styled.main`
+  background-color: greenyellow;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
     <div>
-      <p>Fields</p>
-      <Outlet />
+      <Header />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
-};
+  );
+}
 
 export default AppLayout;
