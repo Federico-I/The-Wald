a7cc7db5aa41f57f8cf93b408fc76b28eeae313d
Federico-I <threxos.48@gmail.com>
2025-06-10 00:36:12 +0000

    Setting-StyleSidebar

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  background-color: aliceblue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <StyledSidebar>Sidebar</StyledSidebar>
   )
 };
 
