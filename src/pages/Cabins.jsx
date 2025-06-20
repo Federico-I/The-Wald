95d843d6742d7f7a7b795a906761363c9bc50b8e
Federico-I <threxos.48@gmail.com>
2025-06-20 20:40:09 +0100

    testinguseEffect-cabinfile

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
+  useEffect(function() {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
