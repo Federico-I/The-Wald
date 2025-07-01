90905709c86261985b543b3981bc7d2e3fe3aa44
Federico-I <threxos.48@gmail.com>
2025-07-01 23:59:13 +0100

    deleteCabin-function-errorHandling

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -15,5 +15,14 @@ export async function deleteCabin(id) {
   const { data, error } = await supabase
     .from("cabins")
     .delete()
-    .eq("some_column", "someValue");
+    .eq("id", id);
+
+    
+  if(error)
+  {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
 };
