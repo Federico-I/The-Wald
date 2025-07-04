dfe509959a9ccfee58d78f6578414a70e1bc5c05
Federico-I <threxos.48@gmail.com>
2025-07-04 21:46:43 +0100

    implementing-toast-notifications

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import GlobalStyles from '../styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +27,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false}/>
+
       <GlobalStyles/> 
       <BrowserRouter>
         <Routes>
@@ -42,6 +44,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <Toaster position='top-center' gutter={12} containerStyle={{ margin: "8px" }} toastOptions={{ success: {duration: 3000},  }}/>
     </QueryClientProvider>
 )
 };
