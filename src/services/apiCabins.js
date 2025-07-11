8aa13672a01e1bd922f0f15df27a94b8efc5c534
Federico-I <threxos.48@gmail.com>
2025-07-11 03:32:31 +0200

    api-Cabins-CreateCabinForm

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,7 +14,7 @@ export async function getCabins() {
 };
 
 export async function CreateCabinForm( newCabin ) {
-  const { data, error } = await supabase.from("cabins").insert([{ some_column: "someValue", other_column: "otherValue"}])
+  const { data, error } = await supabase.from("cabins").insert([newCabin])
 
   if(error)
   {
