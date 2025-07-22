da260ada464f3d85a7fb2bd1e8c861dd5a8a491b
Federico-I <threxos.48@gmail.com>
2025-07-22 23:07:17 +0100

    DinamicError-prop

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -33,10 +33,8 @@ function FormRow({ label, error, children}) {
   return (
     <StyledFormRow>
       <Label htmlFor="name">{label}</Label>
-      <Input type="text" id="name" {...register("name", {
-        required: "This field is requierd",
-      }) }/>
-      {errors?.name?.message && <Error>{errors.name.message}</Error>}
+      {children}
+      {error && <Error>{error}</Error>}
     </StyledFormRow>
   )
 };
