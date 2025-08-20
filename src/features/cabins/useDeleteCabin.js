390e4aa31c4d24cdeb5f988446697b838a0e9d29
Federico-I <threxos.48@gmail.com>
2025-08-20 02:49:39 +0100

    CabinRow-deleteCabin

--- src/features/cabins/useDeleteCabin.js
+++ src/features/cabins/useDeleteCabin.js
@@ -1,13 +1,13 @@
 import react from "react";
 import { toast } from "react-hot-toast";
-import { deleteCabin } from "../../services/apiCabins";
+import { deleteCabin as deleteCabinApi } from "../../services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteCabin() {
   const queryClient = useQueryClient();
 
-  const {isPending: isDeleting, mutate} = useMutation({
-    mutationFn: deleteCabin,
+  const {isPending: isDeleting, mutate: deleteCabin} = useMutation({
+    mutationFn: deleteCabinApi,
     onSuccess: () => {
       toast.success("Cabin successfully deleted");
 
@@ -17,4 +17,6 @@ export function useDeleteCabin() {
     },
     onError: (err) => toast.error(err.message),
   });
-}
+
+  return { isDeleting, deleteCabin };
+};
