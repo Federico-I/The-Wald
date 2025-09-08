867e1f307288fd3129d89e3d389933a932156d2d
Federico-I <threxos.48@gmail.com>
2025-09-08 20:10:28 -0300

    disable-isCreating-button-CabinRow

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -76,7 +76,7 @@ function CabinRow({ cabin }) {
           <Discount>{formatCurrency(discount)}</Discount>
         ) : ( <span>&mdash;</span> )}
         <div>
-          <button onClick={handleDuplicate}>
+          <button disabled={isCreating} onClick={handleDuplicate}>
             <HiSquare2Stack />
           </button>
           <button onClick={() => setShowForm((show) => !show)}>
