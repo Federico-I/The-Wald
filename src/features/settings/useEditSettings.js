8988b3500ee65575970aa042003eb6f4465cc392
Federico-I <threxos.48@gmail.com>
2025-09-15 20:17:13 -0300

    new-useEditSettings

---
+++ src/features/settings/useEditSettings.js
@@ -0,0 +1,20 @@
+import react from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { CreateEditCabinF } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+
+export function useEditCabin() {
+ const queryClient = useQueryClient();
+ 
+ const { mutate: editCabin, isPending: isEditing } = useMutation({
+    mutationFn: ({ newCabinData, id }) => CreateEditCabinF(newCabinData, id),
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabin"] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isEditing, editCabin };
+}
