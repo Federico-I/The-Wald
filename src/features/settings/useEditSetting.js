6d8033270ebea4e3c564cc53f0a693924f97b0e1
Federico-I <threxos.48@gmail.com>
2025-09-17 22:45:29 +0100

    useEditSetting-updateSettingAPI

--- src/features/settings/useEditSetting.js
+++ src/features/settings/useEditSetting.js
@@ -1,16 +1,16 @@
 import react from "react";
 import { useQueryClient } from "@tanstack/react-query";
-import { updateSetting as updateSettingAPI };
+import { updateSetting as updateSettingAPI } from "../../services/apiSettings";
 import toast from "react-hot-toast";
 
-export function useEditSetting() {
+export function useUpdateSetting() {
  const queryClient = useQueryClient();
  
  const { mutate: updateSetting, isPending: isEditing } = useMutation({
-    mutationFn: updateSetting,
+    mutationFn: updateSettingAPI,
     onSuccess: () => {
       toast.success("New cabin successfully created");
-      queryClient.invalidateQueries({ queryKey: ["cabin"] });
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
       reset();
     },
     onError: (err) => toast.error(err.message),
