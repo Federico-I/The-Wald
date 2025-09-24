e8b4d0416161a309d3233057e977097d073402d5
Federico-I <threxos.48@gmail.com>
2025-09-24 02:26:20 +0100

    cabinTable-updatesearch

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
+import { useSearchParams } from "react-router-dom";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -30,6 +31,7 @@ const TableHeader = styled.header`
 
 function CabinTable() {
   const { isPending, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if(isPending) return <Spinner />
 
