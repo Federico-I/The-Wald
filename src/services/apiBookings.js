4da58247d5fac37abbb2a62e4384ae89f588f8c3
Federico-I <threxos.48@gmail.com>
2025-10-13 03:43:19 +0100

    error-handling

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -4,6 +4,10 @@ import supabase from "./supabase";
 
 export async function getBookings () {
   const { data, error } = await supabase.from("bookings").select("*");
+
+  if (error) {
+    throw new Error("Bookings could not be loaded");
+  }
 }
 
 export async function getBooking(id) {
