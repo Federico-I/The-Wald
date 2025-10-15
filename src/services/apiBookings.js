b6e031e8121c845f9afae34df6cfb81f56ca83fe
Federico-I <threxos.48@gmail.com>
2025-10-15 02:00:44 +0100

    async-func-getBooking-await-supabase.select

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -3,7 +3,7 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 export async function getBookings () {
-  const { data, error } = await supabase.from("bookings").select("*, cabins(name), guests(fullName, email)");
+  const { data, error } = await supabase.from("bookings").select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice,cabins(name), guests(fullName, email)");
 
   if (error) {
     throw new Error("Bookings could not be loaded");
