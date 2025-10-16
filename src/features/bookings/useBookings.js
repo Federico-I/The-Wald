478888a089be191444eea50d53a633787950431f
Federico-I <threxos.48@gmail.com>
2025-10-16 23:12:19 +0100

    useBookings-filterValue-status

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,14 @@
 import react from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+
   const {isPending, data: bookings, error} = useQuery({
     queryKey: ["bookings"],
     queryFn: getBookings,
