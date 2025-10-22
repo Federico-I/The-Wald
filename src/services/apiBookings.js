ca86a2d8ea2c396e55f248e6cb067ef7b61d3421
Federico-I <threxos.48@gmail.com>
2025-10-22 22:55:57 +0100

    pagination-count-dynamic

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -21,8 +21,8 @@ export async function getBookings({ filter, sortBy }) {
   // Pagination
 
   if (page) {
-    const from = (page - 1) * (PAGE_SIZE);
-    const to = from + PAGE_SIZE - 1;
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + (PAGE_SIZE - 1);
     
     query = query.range(from, to);    
   } 
