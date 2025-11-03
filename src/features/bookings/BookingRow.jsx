71fbf5f4150278cde01186b5dd932e99b9c8d19e
Federico-I <threxos.48@gmail.com>
2025-11-03 19:48:31 +0000

    checkout-booking-useCheckOut

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,6 +7,8 @@ import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -50,6 +52,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -90,6 +93,10 @@ function BookingRow({
           </Menus.Button>
           {status === "unconfirmed" && ( 
             <Menus.Button icon= {<HiEye />} onClick={() => navigate(`/checkin?${bookingId}`)}>Check in
+            </Menus.Button> 
+          )}
+          {status === "checked-in" && ( 
+            <Menus.Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut}> Check out
             </Menus.Button> 
           )}
         </Menus.List>
---
+++ src/features/check-in-out/useCheckout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateBooking } from '../../services/apiBookings';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
+export function useCheckout() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: checkout, isPending: isCheckingOut } = useMutation({
+    mutationFn: ({ bookingId }) => updateBooking(bookingId, { 
+        status: "checked-out",
+      }
+    ),
+
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClient.invalidateQueries({ active: true }); // 385
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checking in"),
+  });
+
+  return { checkout, isCheckingOut };
+};
