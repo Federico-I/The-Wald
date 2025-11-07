48d701c57a67cdd866da0a94e401d92deb1d0bb6
Federico-I <threxos.48@gmail.com>
2025-11-07 02:43:52 +0000

    navigate-useEffect

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
-import react from 'react';
+import react, { useEffect } from 'react';
 import { useUser } from '../features/authentication/useUser';
 import Spinner from './Spinner';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const FullPage = styled.div`
   height: 100vh;
@@ -12,16 +13,23 @@ const FullPage = styled.div`
 `;
 
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
   // 1 Load the authenticated user
-  const { user, isPending} = useUser();
+  const { user, isPending, isAuthenticated} = useUser();
 
   // 2 wWhile loading, show a spinner
-  if (isPending) return <FullPage>
-    <Spinner />
-  </FullPage>;
+  if (isPending) 
+    return ( 
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
 
   // 3 if there is Noautheticated User, redirect to the /login
-  
+  useEffect(function(){
+    if
+  }, []);
   
   return children;
 };
