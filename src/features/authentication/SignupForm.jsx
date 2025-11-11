52d5cbd202e1e56617ec5ab8e76b716c0ff274e7
Federico-I <threxos.48@gmail.com>
2025-11-11 18:35:16 +0000

    SignUpForm-error-handling

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -7,23 +7,27 @@ import Input from "../../ui/Input";
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues} = useForm();
+  const { register, formState, getValues, handleSubmit} = useForm();
   const { errors } = formState;
+
+  function onSubmit(data) {
+
+  }
 
   return (
     <Form>
-      <FormRow label="Full name" error={""}>
+      <FormRow label="Full name" error={errors?.fullName?.message}>
         <Input type="text" id="fullName" {...register("fullName", { required: "This field is required" })}/>
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
+      <FormRow label="Email address" error={errors?.email?.message}>
         <Input type="email" id="email" {
           ...register("email", 
           { required: "This field is required", pattern: { value: /\S+@\S+\.\S+/, message:"Please provide a valid email address" }, 
         })}/>
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
+      <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
         <Input type="password" id="password" {...register("password", {
           required: "This field is required", 
           value: 8,
@@ -31,7 +35,7 @@ function SignupForm() {
         })}/>
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
+      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
         <Input type="password" id="passwordConfirm" {...register("passwordConfirm", { required: "This field is required", 
           validate: (value) => value === getValues().password || "Passords need to match",
         })}/>
