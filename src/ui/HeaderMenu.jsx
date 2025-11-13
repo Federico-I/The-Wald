525fb61d5ebca45334667598eff7ab376e38e3db
Federico-I <threxos.48@gmail.com>
2025-11-13 03:17:47 +0000

    headerMenu

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  return (
+    <div>HeaderMenu</div>
+  );
+}
+
+export default HeaderMenu;
