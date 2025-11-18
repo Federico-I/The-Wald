46038dd5294407a020249aafac5e7d9a98bbf36b
Federico-I <threxos.48@gmail.com>
2025-11-18 22:17:24 +0000

    updatePasswordForm

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import react from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
 
 function Account() {
   return (
@@ -8,11 +10,11 @@ function Account() {
       <Heading as="h1">Update your account</Heading>
       <Row>
         <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
+        <UpdateUserDataForm />
       </Row>
       <Row>
         <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
+        <UpdatePasswordForm />
       </Row>
     </>
   );
