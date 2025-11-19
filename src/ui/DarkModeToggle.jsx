8ec4bb6f03a0b7f8e0a087588d87ed719966b632
Federico-I <threxos.48@gmail.com>
2025-11-19 21:10:19 +0000

    darkModeToggleButton

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineMoon } from 'react-icons/hi';
+
+function DarkModeToggle() {
+  return (
+    <ButtonIcon>
+      <HiOutlineMoon />
+    </ButtonIcon>
+  )
+}
+
+export default DarkModeToggle;
