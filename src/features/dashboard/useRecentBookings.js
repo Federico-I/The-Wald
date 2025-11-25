8c512259614139fa949ac67038f1c0403cbbdcdd
Federico-I <threxos.48@gmail.com>
2025-11-25 19:45:55 +0000

    queryDate-const

--- src/features/dashboard/useRecentBookings.js
+++ src/features/dashboard/useRecentBookings.js
@@ -1,12 +1,14 @@
 import react from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
+import { getBookingsAfterDate } from '../../services/apiBookings';
 
 export default function useRecentBookings() {
   const [searchParam] = useSearchParams();
 
   const numDays = !searchParam.get("last") ? 7 : Number(searchParam.get("last"));
 
-  return (
-    <div>useRecentBookings</div>
-  )
-};
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+
+}
