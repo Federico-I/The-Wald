ec2981977583f322062a9a839b8be3c42db47705
Federico-I <threxos.48@gmail.com>
2025-12-16 20:03:20 +0000

    implemeting-data

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -143,8 +143,8 @@ function DurationChart({ confirmedStays }) {
     <Heading as="h2">Stay duration Summary</Heading>
     <ResponsiveContainer width="100%" height={240}>
       <PieChart>
-        <Pie data={startDataLight} nameKey="duration" dataKey="value" innerRadius={85} outerRadius={110} cx="40%" cy="50%" paddingAngle={2}>
-          {startDataDark.map((entry) => (<Cell fill={entry.color} stroke={entry.color} key={entry.duration}/>))}
+        <Pie data={data} nameKey="duration" dataKey="value" innerRadius={85} outerRadius={110} cx="40%" cy="50%" paddingAngle={2}>
+          {data.map((entry) => (<Cell fill={entry.color} stroke={entry.color} key={entry.duration}/>))}
         </Pie>
         <Legend verticalAlign="middle" align="right" width="30%" layout="vertical" iconSize={15} iconType="circle"/>
       </PieChart>
