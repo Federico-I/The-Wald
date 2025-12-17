cc2c0060752d44eec340443b4dce505900763c6d
Federico-I <threxos.48@gmail.com>
2025-12-17 21:07:06 +0000

    activitytoady-usequery

--- src/features/check-in-out/useTodayActivity.js
+++ src/features/check-in-out/useTodayActivity.js
@@ -1,7 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
 import react from 'react';
+import { getStaysTodayActivity } from '../../services/apiBookings';
 
 export function useTodayActivity() {
-  
+  const {isLoading, data: stays} = useQuery({
+    queryFn: getStaysTodayActivity,
+    queryKey: ["today-activity"],
+  });
 
   return (
     <div>useTodayActivity</div>
