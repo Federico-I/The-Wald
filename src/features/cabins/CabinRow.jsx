d5030b4e2272340ef92fc6dfd1559050cdf1d300
Federico-I <threxos.48@gmail.com>
2025-12-31 17:28:23 +0000

    bug-CabinRownCleaning

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -12,11 +12,6 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 
 
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-// `;
-
 const Img = styled.img`
   display: block;
   width: 6.4rem;
